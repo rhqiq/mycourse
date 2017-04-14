5116c2b8d80b477431d63fad07bff87bdde2d94f
Reza Haghighi <reza.haghighi@gmail.com>
2017-04-13 23:51:21 -0700

    Solve baloon assignment

---
+++ src/com/rhqiq/seven/Balloon.java
@@ -0,0 +1,40 @@
+package com.rhqiq.seven;
+
+public class Balloon {
+
+    private int maxInflationPressure;
+    private int currentPressure;
+    private boolean isBroken;
+
+    public Balloon(int maxInflationPressure) {
+        this.maxInflationPressure = maxInflationPressure;
+        this.currentPressure = 0;
+        this.isBroken = false;
+    }
+
+    public void inflate(int inputPressure) throws Exception {
+        if (!isBroken){
+            int res = currentPressure + inputPressure;
+            if (res > maxInflationPressure){
+                isBroken = true;
+                throw new Exception("BOOOM! you had too much pressure on your Balloon!");
+            }else if(res < 0){
+                currentPressure = 0;
+            }else{
+                currentPressure = res;
+                System.out.println("Your Balloon was inflated!");
+            }
+        }else{
+            System.out.println("Sorry! Your Balloon has broken!");
+        }
+    }
+
+    public int getCurrentPressure() {
+        return currentPressure;
+    }
+
+
+    public int getMaxInflationPressure() {
+        return maxInflationPressure;
+    }
+}
---
+++ src/com/rhqiq/seven/BalloonTest.java
@@ -0,0 +1,16 @@
+package com.rhqiq.seven;
+
+public class BalloonTest {
+
+    public static void main(String[] args) {
+        Balloon b = new Balloon(100);
+        try{
+            b.inflate(10);
+            b.inflate(40);
+            b.inflate(60);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
