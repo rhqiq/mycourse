b2c2905eaa68aac9da1ffc2aac31b18e887ae301
Reza Haghighi <rezahaghighi@yahoo-inc.com>
2017-05-24 11:24:44 -0700

    [ELENGX436-7]thread

---
+++ src/com/rhqiq/seven/SampleThreads.java
@@ -0,0 +1,45 @@
+package com.rhqiq.seven;
+
+
+public class SampleThreads {
+    int data = 0;
+
+    synchronized void insert(int s) {
+        try {
+            while (data != 0) {
+                // data already holds a string
+                // wait for some other Thread
+                //to remove it!
+                // awaken one other waiting Thread
+                notify();
+                // release the monitor
+                wait();
+            }
+        } catch (InterruptedException e) {
+        }
+        // data is now null
+        data = s;
+        notify();
+    }
+
+    synchronized int extract() {
+        try {
+            while (data == 0) {
+                // no data to extract
+                // wait for some to arrive
+                // awaken one other waiting Thread
+                notify();
+                // release the monitor and go to sleep
+                wait();
+            }
+        } catch (InterruptedException e) {
+        }
+
+        // data is now not full
+        int temp = data;
+        data = 0;
+        notify();
+        return temp;
+    }
+    // more methods
+}
